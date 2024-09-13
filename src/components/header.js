6a987e9c0fe02a12549e628397de1c86f214a805
Siv <var.mironov@gmail.com>
2024-09-13 08:57:53 -0300

    added imgs will polish later

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import { FaInstagram } from 'react-icons/fa'; // Import the Instagram icon
 import logo from '../images/logo.svg';
 
 const Header = () => {
   return (
     <header className="p-4 relative z-10 bg-black" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-      <div className="container mx-auto">
-        <img src={logo} className='max-h-16' />
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <img src={logo} className='max-h-16' alt="Logo" />
+
+        {/* Navbar */}
+        <nav className="flex items-center text-white text-lg space-x-6">
+          {/* Navigation Links */}
+          <a href="#home" className="hover:text-gray-300">Home</a>
+          <span className="h-6 border-r border-gray-400 mx-2"></span> {/* Vertical Line */}
+          <a href="#portfolio" className="hover:text-gray-300">Portfolio</a>
+          <span className="h-6 border-r border-gray-400 mx-2"></span> {/* Vertical Line */}
+          <a href="#about" className="hover:text-gray-300">About</a>
+          <span className="h-6 border-r border-gray-400 mx-2"></span> {/* Vertical Line */}
+          <a href="#contacto" className="hover:text-gray-300">Contacto</a>
+          <span className="h-6 border-r border-gray-400 mx-2"></span> {/* Vertical Line */}
+          
+          {/* Instagram Icon */}
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300">
+            <FaInstagram size={24} /> {/* Increased Icon Size */}
+          </a>
+        </nav>
       </div>
     </header>
-
   );
 };
 
--- src/components/hero.js
+++ src/components/hero.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 // Import your images here
-import bgImage1 from '../images/bpeople.png';
-import bgImage2 from '../images/meeting.png';
-import bgImage3 from '../images/candlechart.png';
-import bgImage4 from '../images/stockgoup.png';
+import bgImage1 from '../images/motherson.jpg';
+import bgImage2 from '../images/glasses.jpg';
+import bgImage3 from '../images/wedding.jpg';
+import bgImage4 from '../images/art.jpg';
+import bgImage5 from '../images/eyepaint.jpg';
+import bgImage6 from '../images/baby.jpg';
 
 const Hero = () => {
-  const images = [bgImage1, bgImage2, bgImage3, bgImage4];
+  const images = [bgImage1, bgImage2, bgImage3, bgImage4, bgImage5, bgImage6];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const moveToNextSlide = useCallback(() => {
@@ -16,12 +18,12 @@ const Hero = () => {
   }, [images.length]);
 
   useEffect(() => {
-    const intervalId = setInterval(moveToNextSlide, 5000);
+    const intervalId = setInterval(moveToNextSlide, 3000);
     return () => clearInterval(intervalId);
   }, [moveToNextSlide]);
 
   return (
-    <main className="flex-grow flex items-start pt-12 relative overflow-hidden">
+    <main className="flex-grow flex items-start pt-12 relative overflow-hidden bg-black">
       <div className="absolute inset-0">
         <AnimatePresence initial={false}>
           <motion.div
